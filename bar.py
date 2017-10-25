190e9b401ebff530980c587b71e53c87b8b73fd8
Jean-Marc Valin <jmvalin@jmvalin.ca>
2017-10-25 02:33:12 -0400

    updated colors to make grayscale printing better

--- bar.py
+++ bar.py
@@ -19,15 +19,15 @@ opacity = 0.4
 error_config = {'ecolor': '0.3'}
 
 rects1 = plt.bar(index-bar_width, cdef, bar_width,
-                 color='b',
+                 color='#000070',
                  label='Prefer CDEF')
 
 rect_tie = plt.bar(index, tie, bar_width,
-                 color='g',
+                 color='#70D050',
                  label='No preference')
 
 rects2 = plt.bar(index + bar_width, none, bar_width,
-                 color='r',
+                 color='#C02020',
                  label='Prefer No CDEF')
 
 clips = ('MINECRAFT', 'Crosswalk', 'DinnerScene', 'TunnelFlag', 'sintel', 'vidyo1')
